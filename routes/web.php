e18729a244839039a88e9866fefc6180c61794cc
xamnes <mriyansyah112@gmail.com>
2026-06-16 20:30:33 +0700

    feat: implement mass upload data functionality using Livewire and PhpSpreadsheet

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/', Dashboard::class)->name('home');
     Route::get('/approvals', App\Livewire\Approvals::class)->name('approvals');
     Route::get('/add-data', App\Livewire\TerdugaForm::class)->name('add-data');
+    Route::get('/upload-data', App\Livewire\UploadData::class)->name('upload-data');
     Route::get('/edit-data/{id}', App\Livewire\TerdugaForm::class)->name('edit-data');
     Route::get('/search', App\Livewire\SearchData::class)->name('search');
     Route::get('/detail/{id}', App\Livewire\TerdugaDetail::class)->name('detail');
