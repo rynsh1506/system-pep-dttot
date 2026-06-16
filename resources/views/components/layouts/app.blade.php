9c3d5714f4cff72f7fdb1a23adda8df42652887f
xamnes <mriyansyah112@gmail.com>
2026-06-16 21:01:38 +0700

    feat: redesign detail and form views, add theme controller and lang switcher

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -31,8 +31,38 @@
                     <span class="font-bold text-primary lg:hidden">PEP & DTTOT</span>
                 </div>
 
-                {{-- Right: user dropdown --}}
-                <div class="flex-none pr-2">
+                {{-- Right: actions & user dropdown --}}
+                <div class="flex-none pr-2 flex items-center gap-2">
+                    
+                    {{-- Language Switcher --}}
+                    <div class="dropdown dropdown-end">
+                        <div tabindex="0" role="button" class="btn btn-sm btn-ghost gap-1 px-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" class="w-4 h-4">
+                                <circle cx="12" cy="12" r="10"></circle>
+                                <line x1="2" y1="12" x2="22" y2="12"></line>
+                                <path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path>
+                            </svg>
+                            <span class="text-xs font-semibold">ID</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-3 h-3 opacity-50">
+                                <path fill-rule="evenodd" d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd"/>
+                            </svg>
+                        </div>
+                        <ul tabindex="0" class="dropdown-content menu menu-sm bg-base-100 rounded-box z-[50] mt-3 w-32 p-2 shadow-lg border border-base-200">
+                            <li><a class="active">Indonesian</a></li>
+                            <li><a>English</a></li>
+                            <li><a>Japanese</a></li>
+                        </ul>
+                    </div>
+
+                    {{-- Theme Controller --}}
+                    <label class="swap swap-rotate btn btn-sm btn-ghost px-2">
+                        <input type="checkbox" class="theme-controller" value="goldendark" />
+                        {{-- Sun icon (visible when light) --}}
+                        <svg class="swap-off fill-current w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z"/></svg>
+                        {{-- Moon icon (visible when dark) --}}
+                        <svg class="swap-on fill-current w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z"/></svg>
+                    </label>
+
                     @php
                         $roleLabels = [1 => 'Staff Input', 2 => 'Supervisor', 3 => 'Manager', 4 => 'Super Admin'];
                         $roleLevel  = session('role_level', 0);
@@ -40,9 +70,9 @@
                         $fullName   = session('full_name', 'User');
                         $initial    = strtoupper(substr($fullName, 0, 1));
                     @endphp
-                    <div class="dropdown dropdown-end">
-                        <div tabindex="0" role="button" class="flex items-center gap-2 cursor-pointer hover:bg-base-200 rounded-xl px-3 py-2 transition-colors">
-                            <div class="bg-primary text-primary-content rounded-full w-9 h-9 flex items-center justify-center font-bold text-sm shrink-0">
+                    <div class="dropdown dropdown-end ml-1">
+                        <div tabindex="0" role="button" class="flex items-center gap-2 cursor-pointer hover:bg-base-200 rounded-xl px-2 py-1.5 transition-colors">
+                            <div class="bg-primary text-primary-content rounded-full w-8 h-8 flex items-center justify-center font-bold text-sm shrink-0">
                                 {{ $initial }}
                             </div>
                             <div class="hidden sm:block text-left">
